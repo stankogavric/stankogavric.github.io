10435754f64bb52e5ad4ee557deb969b9b105d99
Stanko Gavric <gavricstanko@gmail.com>
2021-10-07 21:32:51 +0200

    formatted

--- src/qrCodeAttendance.js
+++ src/qrCodeAttendance.js
@@ -17,39 +17,27 @@ let reload = false;
 
 output = [];
 
-function downloadCsv(){
-  var hiddenElement = document.createElement('a');  
-  hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(output);  
-  hiddenElement.target = '_blank';  
-    
+function downloadCsv() {
+  var hiddenElement = document.createElement('a');
+  hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(output);
+  hiddenElement.target = '_blank';
+
   var today = new Date();
-  var date = ('0' + today.getDate()).slice(-2)+"-"+('0' + (today.getMonth()+1)).slice(-2)+"-"+today.getFullYear();
+  var date = ('0' + today.getDate()).slice(-2) + "-" + ('0' + (today.getMonth() + 1)).slice(-2) + "-" + today.getFullYear();
   var time = ('0' + today.getHours()).slice(-2) + "-" + ('0' + today.getMinutes()).slice(-2) + "-" + ('0' + today.getSeconds()).slice(-2);
-  var dateTime = date+' '+time;
-  hiddenElement.download = "Attendance " + dateTime + '.csv';  
+  var dateTime = date + ' ' + time;
+  hiddenElement.download = "Attendance " + dateTime + '.csv';
   hiddenElement.click();
 }
 
 qrcode.callback = res => {
   if (res) {
-    if (!output.includes(res) && res.length == 10){
+    if (!output.includes(res) && res.length == 10) {
       output.push(res);
       outputData.innerText += res;
       outputData.appendChild(document.createElement("br"));
+      window.scrollTo(0, document.body.scrollHeight);
     }
-    
-    window.scrollTo(0, document.body.scrollHeight);
-
-    /*scanning = false;
-
-    video.srcObject.getTracks().forEach(track => {
-      track.stop();
-    });
-
-    qrResult.hidden = false;
-    canvasElement.hidden = true;
-    btnScanQR.hidden = false;*/
-
     video.play();
     tick();
     scan();
@@ -64,7 +52,6 @@ btnScanQR.onclick = () => {
       track.stop();
     });
 
-    //qrResult.hidden = false;
     canvasElement.hidden = true;
 
     img.src = "./images/reload_icon.svg";
@@ -77,18 +64,16 @@ btnScanQR.onclick = () => {
 
   navigator.mediaDevices
     .getUserMedia({ video: { facingMode: "environment" } })
-    .then(function(stream) {
+    .then(function (stream) {
       img.src = "./images/circle_stop_icon.svg";
       scanning = true;
-      //qrResult.hidden = true;
-      //btnScanQR.hidden = true;
       canvasElement.hidden = false;
       video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
       video.srcObject = stream;
       video.play();
       tick();
-      
-      if (reload){
+
+      if (reload) {
         reload = false;
         output = [];
         outputData.innerText = "";
@@ -103,21 +88,19 @@ function tick() {
   canvasElement.height = video.videoHeight;
   canvasElement.width = video.videoWidth;
   canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-
   scanning && requestAnimationFrame(tick);
 }
 
-function scrollToBottomF(){
+function scrollToBottomF() {
   window.scrollTo(0, document.body.scrollHeight);
   scrollToBottom = false;
 }
 
 function scan() {
   try {
-    if (scrollToBottom){
+    if (scrollToBottom) {
       setTimeout(scrollToBottomF, 1000);
     }
-    
     qrcode.decode();
   } catch (e) {
     setTimeout(scan, 300);
