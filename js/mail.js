4b50ec7325720e7b89960505446a05c833c7e42a
Stanko Gavric <gavricstanko@gmail.com>
2023-05-04 20:06:44 +0200

    no message

--- js/mail.js
+++ js/mail.js
@@ -1,12 +1,62 @@
-function closeModal() {
-  $('#exampleModal').modal('toggle');
-}
-
-function confirm() {
-  let valid = document.querySelector('#subscribe')[0].checkValidity();
-  let form = document.querySelector('#subscribe');
-  if (valid) {
-    form.submit();
-    // closeModal();
-  }
-}
+$(function () {
+  $("#contactForm input").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function ($form, event, errors) { },
+    submitSuccess: function ($form, event) {
+      event.preventDefault();
+      var email = $("input#email").val();
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true);
+      $.ajax({
+        url: "./mail/contact_me.php",
+        type: "POST",
+        data: { email: email },
+        cache: false,
+        success: function () {
+          $("#success").html('<div class="alert alert-success alert-dismissible fade show" role="alert">');
+          $("#success > .alert-success")
+            .html(
+              '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">'
+            )
+            .append("</button>");
+          $("#success > .alert-success").append(
+            "<strong>Your message has been sent. </strong>"
+          );
+          $("#success > .alert-success").append("</div>");
+          $("#contactForm").trigger("reset");
+        },
+        error: function () {
+          $("#success").html('<div class="alert alert-danger alert-dismissible fade show" role="alert">');
+          $("#success > .alert-danger")
+            .html(
+              '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">'
+            )
+            .append("</button>");
+          $("#success > .alert-danger").append(
+            $("<strong>").text(
+              "Sorry" +
+              ", it seems that my mail server is not responding. Please try again later!"
+            )
+          );
+          $("#success > .alert-danger").append("</div>");
+          $("#contactForm").trigger("reset");
+        },
+        complete: function () {
+          setTimeout(function () {
+            $this.prop("disabled", false);
+          }, 1000);
+        },
+      });
+    },
+    filter: function () {
+      return $(this).is(":visible");
+    },
+  });
+  $('a[data-toggle="tab"]').click(function (e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+$("#email").focus(function () {
+  $("#success").html("");
+});
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,21 +1,22 @@
 <?php
 // Check for empty fields
 if(
-   empty($_POST['mail'])      ||
-   !filter_var($_POST['mail'],FILTER_VALIDATE_EMAIL))
+   empty($_POST['email'])     ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
    return false;
    }
-// Send email
-$email_address = strip_tags(htmlspecialchars($_POST['mail']));
+   
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+   
 // Create the email and send the message
 $to = 'gavricstanko@gmail.com';
-$email_subject = "Website luminous.rs";
-$email_body = "You have received a new subscriber from your website.\n\n"."Here are the details:\n\nEmail: $email_address";
-$headers = "From: office@luminous.rs\n";
-$headers .= "Reply-To: $email_address\n";   
-$headers .= "Content-Type: text/plain; charset=UTF-8";
+$email_subject = "New subscription";
+$email_body = "You have received a new subscription from your website.\n\n"."Here are the details:\n\nEmail: $email_address";
+$headers = "From: noreply@luminous.rs\n";
+$headers .= "Reply-To: $email_address\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8";   
 mail($to,$email_subject,$email_body,$headers);
-return true; 
+return true;         
 ?>
